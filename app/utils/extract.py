466d175ad0c7c13d4540502f10f4e445f3321ef2
Quentin <quentin.loridant@protonmail.com>
2022-05-04 14:33:55 -0500

    Handle the case where cookies are not asked in the firefox sessions

--- app/utils/extract.py
+++ app/utils/extract.py
@@ -5,12 +5,19 @@ import logging
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import ElementNotInteractableException, StaleElementReferenceException
+from selenium.common.exceptions import ElementNotInteractableException, StaleElementReferenceException, NoSuchElementException
 from webdriver_manager.firefox import GeckoDriverManager
 from collections import defaultdict
 
 class DateAttributeError(Exception):
     pass
+
+def accept_eventual_cookies(driver):
+    driver.implicitly_wait(3)
+    try: # In case the website asks to valide cookies for the session
+        driver.find_element(By.XPATH, "//input[@type='submit' and @value='Accepter']").click()
+    except NoSuchElementException:
+        pass
 
 def extract_url_dl(no_browser=True, start_date=datetime(2016, 3, 30), end_date=datetime(2016, 4, 3)) -> {}:
     """
@@ -35,8 +42,9 @@ def extract_url_dl(no_browser=True, start_date=datetime(2016, 3, 30), end_date=d
 
     # Load the page, accept cookies and click on the download section
     driver.get(url_time_publishment)
-    driver.implicitly_wait(3)
-    driver.find_element(By.XPATH, "//input[@type='submit' and @value='Accepter']").click()
+
+    accept_eventual_cookies(driver)
+
     driver.find_elements(By.CSS_SELECTOR, ".telechargements")[3].click()
 
     datepicker = driver.find_element(By.ID, 'datepicker')
@@ -50,6 +58,7 @@ def extract_url_dl(no_browser=True, start_date=datetime(2016, 3, 30), end_date=d
 
         massifs = driver.find_element(By.ID, 'select_massif').find_elements(By.XPATH, '//option')
         for option in massifs:
+            print(option.text)
             try:
                 option.click()
             except StaleElementReferenceException:
