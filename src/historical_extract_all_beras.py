a4ff4d8b17f6516eb4eb8c0b1af61d21e1699cb6
Quentin <quentin.loridant@protonmail.com>
2022-09-17 17:33:58 +0200

    Use poetry

--- src/historical_extract_all_beras.py
+++ src/historical_extract_all_beras.py
@@ -8,10 +8,11 @@ logger = init_logger(logging.DEBUG)
 
 branch = 'master'
 repo = init_repo()
-
+MASSIFS=[MASSIFS[0]]
 for massif in MASSIFS:
     # Lecture de la date de publication de notre fichier
     dates_ = subprocess.run(["cat", f"src/data/{massif}/urls_list.txt"], capture_output=True).stdout.decode('utf-8').split('\n')
+    dates_ = [dates_[0]]
     logger.debug(massif)
     new_data = []
     for date_ in dates_:
@@ -28,4 +29,4 @@ for massif in MASSIFS:
 
     file_path = f'src/data/{massif}/hist.csv'
     logger.info(f'Exporting the BERA to Github for massif : {massif}   ...')
-    push(repo, file_path, "Daily automatic file update", new_data, branch, update=True, type_data='bera')
+    # push(repo, file_path, "Daily automatic file update", new_data, branch, update=True, type_data='bera')
--- tests/utils/test_bulletin.py
+++ tests/utils/test_bulletin.py
@@ -1,8 +1,8 @@
 import pytest
-from mock import patch, PropertyMock
 import xml.etree.ElementTree as ET
 
-from utils.bulletin import Bulletin, MassifInexistantException
+from mock import patch, PropertyMock
+from src.utils.bulletin import Bulletin, MassifInexistantException
 
 
 def test_init():
@@ -10,8 +10,8 @@ def test_init():
     assert valid_bu.jour == '20160417132702'
     assert valid_bu.massif == 'CHARTREUSE'
 
-    with pytest.raises(MassifInexistantException) as e_info:
-        invalid_massif = Bulletin('CHARTREUX', '20160417132702')
+    with pytest.raises(MassifInexistantException):
+        Bulletin('CHARTREUX', '20160417132702')
 
 
 def test_jour_key():
@@ -20,18 +20,13 @@ def test_jour_key():
     assert len(bu.jour_key) == 10
     assert (bu.jour_key[4] == '-' and bu.jour_key[7] == '-')
     assert bu.jour_key == '2016-04-17'
-
     assert bu.jour_key != '20160417132702'
 
-def test():
-    return ET.parse('tests/valid_bera.xml')
-
-def mock_et_parse():
-    return test
 
 def test_parse():
     bu = Bulletin('VERCORS', '20200517132702')
-    with patch('utils.bulletin.Bulletin.path_file', new_callable=PropertyMock) as a:
+    with patch('src.utils.bulletin.Bulletin.path_file',
+               new_callable=PropertyMock) as a:
         a.return_value = 'tests/valid_bera.xml'
         risques = bu.parse()
         assert len(risques) == 9
@@ -44,28 +39,13 @@ def test_parse():
         assert 'LOC2' in risques
         assert 'RISQUEMAXI' in risques
         assert 'COMMENTAIRE' in risques
-    with patch('utils.bulletin.Bulletin.path_file', new_callable=PropertyMock) as a:
+    with patch('src.utils.bulletin.Bulletin.path_file',
+               new_callable=PropertyMock) as a:
         a.return_value = 'tests/invalid_tag_bera.xml'
-        with pytest.raises(ET.ParseError) as e_info:
-            risques = bu.parse()
-    with patch('utils.bulletin.Bulletin.path_file', new_callable=PropertyMock) as a:
+        with pytest.raises(ET.ParseError):
+            bu.parse()
+    with patch('src.utils.bulletin.Bulletin.path_file',
+               new_callable=PropertyMock) as a:
         a.return_value = 'tests/invalid_attribute_bera.xml'
-        with pytest.raises(ET.ParseError) as e_info:
-            risques = bu.parse()
-
-
-
-    # mocker.patch('xml.etree.ElementTree.parse', new_callable=mock_et_parse)
-    # bu.parse()
-
-    #
-    # def parse(self):
-    #     root = ET.parse('app/tmp_bera.xml').getroot()
-    #     self.cartouche_risque = root[0].find('CARTOUCHERISQUE')
-    #     self.risques = self.cartouche_risque[0].attrib
-    #     logger.debug(self.risques)
-    #
-    # def append_csv(self):
-    #     # Removing comma as we will save the file as a csv
-    #     risques = list(map(lambda x: x.replace(',', '-'), self.risques.values()))
-    #     return [self.jour_key, self.massif, *risques]
+        with pytest.raises(ET.ParseError):
+            bu.parse()
