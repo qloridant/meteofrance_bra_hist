0e696fabade005fe3fda08b4b4dd4f4f69b80e12
Amélie Rondot <amelie.rondot@multi.coop>
2023-03-30 15:44:10 +0200

    Add doc for scripts daily_build_url.py and daily_extract_all_beras.py

--- bera/daily_extract_all_beras.py
+++ bera/daily_extract_all_beras.py
@@ -1,3 +1,43 @@
+"""
+This script aims to add new BERA information from the last BERA published for each mountain chain, and save it into all
+the data/<massif>/hist.csv files in the 'master' branch of the GitHub repository
+https://github.com/qloridant/meteofrance_bra_hist.
+
+This script is executed daily after the execution of the script daily_build_url.py by the GitHub main action defined in
+the .github/workflows/main.yml file of this project.
+
+This scripts:
+
+- for each chain mountain:
+
+    - reads the file data/<massif>/urls_list.txt in the GitHub repository
+      https://github.com/qloridant/meteofrance_bra_hist to get the last datetime written in it. This datetime
+      corresponds to the last datetime of BERA publication for this chain mountain,
+
+    - downloads the last BERA available in xml format thanks to the th last datetime of BERA publication get previously,
+
+    - parses the xml content into a list of datas (as date, chain mountain, avalanche risk, evolution of the risk with
+    altitude, eventuals comments, url to download the BERA in pdf format), for example
+      ['2023-03-29', 'CHABLAIS', '2', '', '<2500', '2500', '3', '', '>2500', '3',
+       'Au-dessus de 2500 m : Risque marqué. En-dessous : Risque limité.',
+       'https://donneespubliques.meteofrance.fr/donnees_libres/Pdf/BRA/BRA.CHABLAIS.20230329140534.pdf']
+
+    - gets the file data/<massif>/hist.csv in the GitHub repository
+      https://github.com/qloridant/meteofrance_bra_hist,
+
+    - updates the data/<massif>/hist.csv file content by adding this list of new datas at the beginning of the
+      actual content,
+
+- creates a git commit including all updated files,
+
+- pushes the git commit in the 'master' branch of the GitHub repository
+https://github.com/qloridant/meteofrance_bra_hist
+
+This script could be launched in a development context, independently of the GitHub action, but it should be executed
+after the execution of the script daily_build_urls.py to get the new BERA datetime publication.
+Cf README.md in section 'Developpement' to launch this script in a development context.
+"""
+
 import os
 import subprocess
 
