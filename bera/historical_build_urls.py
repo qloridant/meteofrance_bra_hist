d84e8f154e847c6bc4092ee2b2a1c182806739ec
Amélie Rondot <amelie.rondot@multi.coop>
2023-04-06 14:15:17 +0200

    Edit historical_build_url script to save urls_list.txt files locally

--- bera/historical_build_urls.py
+++ bera/historical_build_urls.py
@@ -10,43 +10,42 @@ data/<massif>/urls_list.txt files and this script is not useful anymore.
 
 This script does the same as the script daily_build_urls.py but for each day of the given period.
 """
-
+import logging
 import os
+import time
 from datetime import datetime
 
 from utils.common import init_logger, MASSIFS
 from utils.extract import extract_url_dl
-from utils.github_utils import init_repo, commit_many_files_and_push, \
-    update_and_add_file_to_commit
+from utils.github_utils import init_repo, update_file_content
 
-logger = init_logger()
+logger = init_logger(logging.DEBUG)
 
 if __name__ == '__main__':
+    start_time = time.time()
     branch = os.getenv('GIT_BRANCH_NAME')
     if not branch:
         raise Exception(
             'Unknown environment variable GIT_BRANCH_NAME - Stopping here  ...')
 
-    logger.info('Starting the extraction of urls...')
+    logger.info(f'{time.time() - start_time} seconds - Starting the extraction of urls ...')
     new_urls = extract_url_dl(no_browser=True,
-                              start_date=datetime(2023, 1, 10),
-                              end_date=datetime(2023, 1, 25))
+                              start_date=datetime(2023, 1, 25),
+                              end_date=datetime(2023, 1, 27))
 
     repo = init_repo()
-    files_to_commit = []
 
     for massif in MASSIFS:
         file_path = f'data/{massif}/urls_list.txt'
-        logger.info(f'Exporting the URL to Github for massif : {massif}   ...')
+        logger.debug(f'{time.time() - start_time} seconds  - Exporting the URL for massif : {massif}   ...')
 
-        # Update and add files to commit
-        files_to_commit = update_and_add_file_to_commit(repo, file_path,
-                                                        branch,
-                                                        new_urls[massif],
-                                                        'url', files_to_commit)
+        # Update file
+        full_content = update_file_content(repo, file_path, branch, new_urls[massif], 'url')
 
-    logger.info('Compile all modified files in one commit  ...')
-    commit_many_files_and_push(repo, branch,
-                               "Historical automatic url files update",
-                               files_to_commit)
-    logger.info('Job succeeded  ...')
+        # Save file locally
+        with open(file_path, 'w') as f:
+            f.write(full_content)
+        logger.debug(f'{time.time() - start_time} seconds - Successfully writing the URLs to urls_list.txt file '
+                     f'for massif: {massif}   ...')
+
+    logger.info(f'{time.time() - start_time} seconds - Job succeeded  ...')
